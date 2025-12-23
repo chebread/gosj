ce9982f2522e14129a57936ddd7c3f03a75b9f9e
chebread <83535893+chebread@users.noreply.github.com>
2025-12-23 20:36:23 +0900

    blank

--- main.go
+++ main.go
@@ -16,9 +16,19 @@ func main() {
 		Name:  "Alice",
 		IsMan: false,
 	}
-	data, err := json.Marshal(x)
+	data, err := json.Marshal(x) // byte slice
 	if err != nil {
 		log.Fatalf("JSON marshaling failed: %s", err)
 	}
-	fmt.Printf("%s\n", data)
+
+	var y struct{ Released string }
+	// 필드 태그로 인해 released로 바뀌었으므로,
+	// 받을 필드명을 released로 해야 하며,
+	// 대문자로 시작해야 하니 Released로 해야 한다.
+	// 이렇게 하기 실핟면 필드 태그를 활용하라.
+	if err := json.Unmarshal(data, &y); err != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", err)
+	}
+
+	fmt.Printf("%s\n", y)
 }
